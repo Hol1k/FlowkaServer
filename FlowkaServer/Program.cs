3a61719332ac7d0f9df67a83b7dd99515ed55c3c
Hol1k <danilugryumov96@gmail.com>
2025-07-14 17:48:29 +0500

    allowed outbound connections

--- FlowkaServer/Program.cs
+++ FlowkaServer/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:5080");
+
 builder.Services.AddDbContext<FlowkaContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
