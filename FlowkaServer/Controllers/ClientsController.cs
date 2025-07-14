35277ef847cd69ef4c012a60693d43c55fe58c3a
Hol1k <danilugryumov96@gmail.com>
2025-07-14 13:02:56 +0500

    added clientController and connected swagger

---
+++ FlowkaServer/Controllers/ClientsController.cs
@@ -0,0 +1,31 @@
+﻿using FlowkaDbContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowkaServer.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ClientsController : ControllerBase
+{
+    private readonly FlowkaContext _db;
+
+    public ClientsController(FlowkaContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public IActionResult GetClients()
+    {
+        var clients = _db.Clients.ToList();
+        return Ok(clients);
+    }
+
+    [HttpPost]
+    public IActionResult CreateClient([FromBody] ClientEntity client)
+    {
+        _db.Clients.Add(client);
+        _db.SaveChanges();
+        return CreatedAtAction(nameof(GetClients), new { Id = client.Id }, client);
+    }
+}
--- FlowkaServer/Program.cs
+++ FlowkaServer/Program.cs
@@ -6,4 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<FlowkaContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
